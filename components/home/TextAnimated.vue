3927f409563fe6262a3d5e464d37f7253fc13ff4
Joyfer <joyferramos@gmail.com>
2021-07-04 20:12:24 -0600

    Added text transition

--- components/home/TextAnimated.vue
+++ components/home/TextAnimated.vue
@@ -1,40 +1,32 @@
 <template>
   <div class="d-flex justify-center flex-column full-width text-center">
     <span class="text-h4 mb-3">¿Pensando en...</span>
-
-    <transition-group name="list" mode="in-out" class="height">
-      <h2
-        v-for="(el, index) in textList"
-        :key="el"
-        class="text-h2 text__title-purple mb-3 primary--text pb-3 height"
-        v-text="`${el}?`"
-        v-show="i === index"
-      ></h2>
-    </transition-group>
+    <client-only>
+    <vue-typer
+      class="text-h2 text__title-purple mb-3 primary--text pb-3 height"
+      :text="textList"
+      erase-style='select-back'
+      :erase-delay='60'
+    ></vue-typer>
+    </client-only>
   </div>
 </template>
 
 <script>
 export default {
   name: "TextAnimated",
+  components: {},
   data() {
     return {
-      text: "aaaa",
-      textList: ["dominar la web", "llegar más lejos", "traspasar fronteras", "invertir en ti"],
-      i: 0,
+      textList: [
+        "dominar la web?",
+        "llegar más lejos?",
+        "traspasar fronteras?",
+        "invertir en ti?",
+      ],
     };
   },
   methods: {},
-  mounted() {
-    let interval = setInterval(() => {
-      if (this.i > 2) {
-        return;
-      }
-      console.log(this.i);
-      this.text = this.textList[this.i];
-      this.i++;
-    }, 3000);
-  },
 };
 </script>
 
@@ -42,24 +34,7 @@ export default {
 .full-width {
   width: 100%;
 }
-.list-item {
-  display: inline-block;
-  margin-right: 10px;
-}
-.list-enter-active {
-  transition: all 1s;
-}
-.list-leave-active {
-  transition: all 0s;
-}
-.list-leave-to {
-  opacity: 0;
-}
-.list-enter /* .list-leave-active below version 2.1.8 */ {
-  opacity: 0;
-  transform: translateX(-30px);
-}
-.height {
-  height: 80px;
+.vue-typer >>> span {
+  color: #6b63ff;
 }
 </style>
